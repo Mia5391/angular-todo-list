4f4f38bf61e3942e9eb24c6554595aafc685cd44
Mia <mariakrasteva5391@gmail.com>
2023-09-02 23:39:05 +0300

    refactor: extract task list to its own freshly created component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,37 +7,7 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'To Do List by Mia';
-  tasks: Task[] = [
-    new Task("Visit Ann"), 
-    new Task("Cook Dinner"),
-    new Task("Wash Dishes"),
-    new Task("Go to the gym"),
-    new Task("Party Shopping"),    
-  ]
-
-  add(newTask: string){
-    this.tasks.push(new Task(newTask))
-  }
-  remove(existingTask: Task){
-    var userConfirmed = confirm(`Are you sure that you want to remove the following task? \n "${existingTask}"`)
-
-    if(userConfirmed){
-      this.tasks = this.tasks.filter(task => task != existingTask);
-    }  
-  }
-
-}
-
-class Task {
-  constructor(public title: string){
-
-  }
-  toggleIsDone(){
-    this.isDone = !this.isDone;
-
-  }
-
-  public isDone = false;
+  
 }
 
 
