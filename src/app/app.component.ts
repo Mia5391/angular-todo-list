72284be93d37ca10baa9439c88441f0d06e79e10
Mia <mariakrasteva5391@gmail.com>
2023-09-02 15:21:16 +0300

    refactor: load tasks dynamically from component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'todo-list';
+  title = 'To Do List by Mia';
+  tasks = [
+    "Visit Ann", 
+    "Cook Dinner",
+    "Wash Dishes",
+    "Go to the gym",
+    "Party Shopping"
+  ]
 }
